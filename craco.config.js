93ed29be437ff053a7041802bc716f61e85f47ff
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-10-25 18:41:59 +0300

    1.0.1
    edit craco.config.js:
      remove excess alias,
      add src/Pages (with alias),
      edit spacing

--- craco.config.js
+++ craco.config.js
@@ -3,14 +3,14 @@ const path = require('path');
 module.exports = {
 	webpack: {
 		alias: {
-
 			'@src': path.resolve(__dirname, 'src'),
+
 			'@components': path.resolve(__dirname, 'src', 'Components'),
+			'@pages': path.resolve(__dirname, 'src', 'Pages'),
+
 			'@stateSlices': path.resolve(__dirname, 'src', 'app', 'stateSlices'),
-			'@initState': path.resolve(__dirname, 'src', 'app', 'initState'),
 
 			'@imgs': path.resolve(__dirname, 'src', 'imgs')
-
 		}
 	}
 }
